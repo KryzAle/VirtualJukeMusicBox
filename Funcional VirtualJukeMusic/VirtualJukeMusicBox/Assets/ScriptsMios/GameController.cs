286f7e0d67cbab6bac01c7577187c76c2a2a197b
Alexander Arcos <alexanderarcos66@gmail.com>
2019-06-04 20:11:30 -0500

    Creacion Esena 1
    se creo la versión 1.0 de la primera esena con la generacion de bloques aleatorios

---
+++ Funcional VirtualJukeMusic/VirtualJukeMusicBox/Assets/ScriptsMios/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public Vector3 spawnValues2;
+    public Vector3 spawnValues3;
+    public int hazardCound;
+    private float spawnWait = 1.22f;
+    private float spawnWait2;
+    private float startWait = 0.22f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3();
+        yield return new WaitForSeconds(startWait);
+        for (int i = 0; i < hazardCound; i++)
+        {
+
+            spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0, spawnValues.z);
+            Instantiate(hazard, spawnPosition, Quaternion.identity);
+
+
+
+            yield return new WaitForSeconds(spawnWait);
+
+        }
+
+    }
+}
---
+++ Funcional VirtualJukeMusic/VirtualJukeMusicBox/Assets/ScriptsMios/Lanzar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lanzar : MonoBehaviour
+{
+    public float visionRadius;
+    public float speed;
+    GameObject player;
+    Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision otherObj)
+    {
+        if (otherObj.gameObject.tag == "laser")
+        {
+            Destroy(otherObj.gameObject, .1f);
+        }
+
+    }
+}
