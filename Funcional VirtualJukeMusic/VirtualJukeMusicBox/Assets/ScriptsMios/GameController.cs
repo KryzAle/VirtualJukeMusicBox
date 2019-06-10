4656c6d5c2742e5429f150ee008f7b4714fef603
Alexander Arcos <alexanderarcos66@gmail.com>
2019-06-09 19:05:47 -0500

    Finalizar Cancion1
    Se termino la cancion 1

--- Funcional VirtualJukeMusic/VirtualJukeMusicBox/Assets/ScriptsMios/GameController.cs
+++ Funcional VirtualJukeMusic/VirtualJukeMusicBox/Assets/ScriptsMios/GameController.cs
@@ -19,7 +19,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     IEnumerator SpawnWaves()
     {
-        
+        //Intro
         yield return new WaitForSeconds(startWait);
         for (int i = 0; i < 2; i++)
         {
@@ -46,8 +46,10 @@ public class GameController : MonoBehaviour
         CrearDerecha();
         yield return new WaitForSeconds(0.667f);
         CrearIzquierda();
+        //Estribillo
         yield return new WaitForSeconds(0.14f);
         CrearIzquierda();
+        yield return new WaitForSeconds(0.3f);
         for (int i = 0; i < 3; i++)
         {
             yield return new WaitForSeconds(0.31f);
@@ -69,17 +71,148 @@ public class GameController : MonoBehaviour
         CrearIzquierda();
         yield return new WaitForSeconds(0.20f);
         CrearDerecha();
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.2f);
+        for (int i = 0; i < 11; i++)
+        {
+            yield return new WaitForSeconds(0.31f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.31f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.31f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(0.31f);
+            CrearIzquierda();
+        }
+        //Coro
+        yield return new WaitForSeconds(0.5f);
+        CrearIzquierda();
+               
+        for (int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(0.54f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.54f);
+            CrearDerecha();
+            yield return new WaitForSeconds(1.3f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.54f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(0.54f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(1.3f);
+            CrearIzquierda();
+        }
+        //Estrofa
+        yield return new WaitForSeconds(3f);
+        for (int i = 0; i < 2; i++)
+        {
+            CrearIzquierda();
+            yield return new WaitForSeconds(spawnWait);
+            CrearDerecha();
+            yield return new WaitForSeconds(spawnWait);
+        }
+        yield return new WaitForSeconds(2.422f);
+        CrearIzquierda();
+        yield return new WaitForSeconds(2.65f);
+        CrearIzquierda();
+        yield return new WaitForSeconds(1.48f);
+        CrearDerecha();
+        yield return new WaitForSeconds(1.20f);
+        CrearIzquierda();
+        yield return new WaitForSeconds(1.20f);
+        CrearDerecha();
+        yield return new WaitForSeconds(1.20f);
+        CrearIzquierda();
         for (int i = 0; i < 6; i++)
         {
-            
-            CrearIzquierda();
-            yield return new WaitForSeconds(0.31f);
-            CrearIzquierda();
+            yield return new WaitForSeconds(2f);
+            if (i%2==0)
+            {
+                CrearDerecha();
+            }
+            else
+            {
+                CrearIzquierda();
+            }
+        }
+        //Repite Estribillo
+        yield return new WaitForSeconds(1.5f);
+        CrearDerecha();
+        yield return new WaitForSeconds(0.667f);
+        CrearIzquierda();
+        yield return new WaitForSeconds(0.14f);
+        CrearIzquierda();
+        yield return new WaitForSeconds(0.3f);
+        for (int i = 0; i < 3; i++)
+        {
             yield return new WaitForSeconds(0.31f);
             CrearDerecha();
             yield return new WaitForSeconds(0.31f);
             CrearDerecha();
+            yield return new WaitForSeconds(0.31f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(0.31f);
+            CrearIzquierda();
+        }
+        yield return new WaitForSeconds(0.31f);
+        CrearDerecha();
+        yield return new WaitForSeconds(0.31f);
+        CrearDerecha();
+        yield return new WaitForSeconds(0.43f);
+        CrearDerecha();
+        yield return new WaitForSeconds(0.20f);
+        CrearIzquierda();
+        yield return new WaitForSeconds(0.20f);
+        CrearDerecha();
+        yield return new WaitForSeconds(0.2f);
+        for (int i = 0; i < 11; i++)
+        {
+            yield return new WaitForSeconds(0.31f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.31f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.31f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(0.31f);
+            CrearIzquierda();
+        }
+        //Repite Coro
+        yield return new WaitForSeconds(0.5f);
+        CrearIzquierda();
+
+        for (int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(0.54f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.54f);
+            CrearDerecha();
+            yield return new WaitForSeconds(1.3f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.54f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(0.54f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(1.3f);
+            CrearIzquierda();
+        }
+        //Repite Coro 2
+        yield return new WaitForSeconds(3f);
+        CrearIzquierda();
+
+        for (int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(0.54f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.54f);
+            CrearDerecha();
+            yield return new WaitForSeconds(1.3f);
+            CrearDerecha();
+            yield return new WaitForSeconds(0.54f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(0.54f);
+            CrearIzquierda();
+            yield return new WaitForSeconds(1.3f);
+            CrearIzquierda();
         }
 
     }
