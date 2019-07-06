4c52eab406658a4518722e3f7c4da5333dabde8f
Alexander Arcos <alexanderarcos66@gmail.com>
2019-07-05 20:51:13 -0500

    jukeBox V0.2
    Se Agrego la tabla de puntuación en el menú de inicio y se mostró el score en el gamer over.

---
+++ Funcional VirtualJukeMusic/VirtualJukeMusicBox/Assets/ScriptsMios/bdGameOver.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using UnityEngine.UI;
+
+public class bdGameOver : MonoBehaviour
+{
+    public Text score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Listar();
+    }
+    public void Listar()
+    {
+       
+        string conn = "URI=file:" + Application.dataPath + "/Plugins/Datos.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT MAX(id),* FROM Persona";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            
+            double scorelb = reader.GetDouble(3);
+
+            score.text = scorelb.ToString();
+
+        }
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
